292ad764c8348d277b7230afca8477e15061b34e
Pavan543210 <135861719+Pavan543210@users.noreply.github.com>
2024-05-07 00:15:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import requests
+
+# Streamlit App
+st.title("Named Entity Recognition")
+
+# Text area for user input
+sample_sentence = st.text_area("Enter a sample sentence:", "")
+
+# Button to trigger named entity recognition
+if st.button("Recognize Entities"):
+    # API URL and headers
+    API_URL = "https://api-inference.huggingface.co/models/pavanch121/distilbert-base-uncased-finetuned-ner"
+    headers = {"Authorization": "Bearer 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"}
+
+    # Function to make API call
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    # Perform API call for named entity recognition
+    output = query({"inputs": sample_sentence})
+
+    # Extract and display word and entity group tag pairs
+    st.write("Word-Entity Group Pairs:")
+    for entity in output:
+        word = entity['word']
+        entity_group = entity['entity_group']
+        st.write(f"{word} - {entity_group}")
